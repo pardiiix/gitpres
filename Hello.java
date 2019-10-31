3522e1f074a4917d31ebeef01dbe774e47810ffd
avery <averypeckstl@gmail.com>
2019-10-31 02:58:46 -0500

    Add Java example

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+  public static void main(String[] args) {
+    //modify this file so that instead of printing a hard-coded "Hello Student!, it prints out what a user types in
+    String name = "Student";
+    System.out.println("Hello " + name + "!");
+  }
+}
